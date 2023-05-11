0b85aab99b8e65554fc73a9986096daaceb9b994
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-11 15:50:20 +0700

    update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+      // mencari anak yang nilai nya lebih dari 60
+      const nilai = [40, 50, 60, 70, 80, 90, 100];
+      const siswa = [3, 5, 11, 15, 3, 1, 2];
+      let jumlahSiswa = 0;
+      for (let i = 0; i < nilai.length; i++) {
+        if (nilai[i] > 60) {
+          jumlahSiswa += siswa[i];
+        }
+      }
+      console.log(
+        `Jumlah siswa yang mendapatkan nilai lebih dari 60 adalah: ${jumlahSiswa}`
+      );
