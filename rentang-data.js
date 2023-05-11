322bcf42ee1dc063b4330d94e7842fd7544af4a7
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-11 15:52:16 +0700

    update rentang-data.js

---
+++ rentang-data.js
@@ -0,0 +1,9 @@
+      // Matematika
+      let data = [62, 35, 45, 47, 56, 58];
+      // mencari nilai terendah
+      let nilaiMin = Math.min(...data);
+      // mencari nilai tertinggi
+      let nilaiMax = Math.max(...data);
+      // menghitung rentang data
+      let rentang = nilaiMax - nilaiMin;
+      console.log("Rentang data : " + rentang);
